6f14f61555ba4868bcf2f2a70461b1fb488bb9a2
Alvin <alvin@episunsa.edu.pe>
2016-09-25 21:52:15 -0500

    page login

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,7 +1,16 @@
 var express = require('express');
 var router = express.Router();
-var ctrlMain = require('../controllers/main');
-/* GET home page. */
-router.get('/',ctrlMain.index);
+var ctrlLocations = require ('../controllers/locations');
+var ctrlOthers = require ('../controllers/others');
+var ctrlLogin = require ('../controllers/login');
+/*login page*/
+router.get('/login',ctrlLogin.login);
+/* Location pages. */
+router.get('/',ctrlLocations.homeList);
+router.get('/location',ctrlLocations.locationInfo);
+router.get('/location/review/new',ctrlLocations.addReview);
+
+/*Other pages */
+router.get('/about',ctrlOthers.about);
 
 module.exports = router;
