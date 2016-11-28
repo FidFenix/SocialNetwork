17241433c56f74dcc50469bc7483f8e586f66160
Alvin <alvin@episunsa.edu.pe>
2016-11-28 02:03:04 -0500

    deleting trash

--- app_server/models/locations.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require ('mongoose');
-var locationSchema = new mongoose.Schema({
-    name:{type:String,required:true},
-    adress:String,
-    rating:{type: Number,"default": 0,min: 0,max: 5},
-    facilities:[String],
-    coord:{type:[Number],index:'2dsphere'}
-});
